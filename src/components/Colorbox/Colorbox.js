449d84c5ac70383791aaa64323f01bcb65d08609
lsikose09 <lino.sikosek@student.tugraz.at>
2021-02-25 10:16:09 +0100

    Refactored color update with useEffect

--- src/components/Colorbox/Colorbox.js
+++ src/components/Colorbox/Colorbox.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Colorbox.css';
 import COLORS from '../../shared/colors';
 
-const Colorbox = props => {
+const Colorbox = ({ onInput }) => {
 
     const [color, setColor] = React.useState(COLORS.defaultColor);
 
     const changeColor = e => {
         setColor(e.target.value);
-        props.onChange(color);
     }
+    
+
+    useEffect(() => {
+        onInput(color);
+    }, [color, onInput]);
 
     return (
         <div className="tooltip-container">
-            <input type="color" value={color} id="colorbox" name="head" onChange={changeColor}/>
+            <input type="color" value={color} id="colorbox" name="head" onInput={changeColor}/>
         </div>
     );
 }
